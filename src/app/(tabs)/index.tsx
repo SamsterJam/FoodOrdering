aee5fdafe88017a8555bc8b289fe35218df96507
SamsterJam <samsterjam@gmail.com>
2024-05-18 15:31:16 -0400

    added flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import { View,} from 'react-native';
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
+import { View, FlatList} from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
-    </View>
+      <FlatList 
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item}/>}
+      />
   );
 }
 
