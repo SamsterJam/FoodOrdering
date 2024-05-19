6877e124654b0817dcc57b6603f56296ed07383c
SamsterJam <samsterjam@gmail.com>
2024-05-18 22:08:24 -0400

    Simple Details Page

---
+++ src/app/product.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function ProductDetailsScreen(){
+    return(
+        <View>
+            <Text>Product Details Here...</Text>
+        </View>
+    )
+}
