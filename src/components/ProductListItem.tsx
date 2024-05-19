bd60e54362800deb5a2ef6733f0dd47d06ff9822
SamsterJam <samsterjam@gmail.com>
2024-05-18 21:43:16 -0400

    Minor UI improvments

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -12,7 +12,7 @@ export default function ProductListItem({product}: ProductListItemProps) {
   return (
     <View style={styles.container}>
 
-      <Image source={{uri : product.image || defaultPizzaImage}} style={styles.image}/>
+      <Image source={{uri : product.image || defaultPizzaImage}} style={styles.image} resizeMode='contain'/>
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>{product.price}</Text>
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 20,
     flex: 1,
+    maxWidth: '50%',
   },
   title: {
     fontSize: 18,
