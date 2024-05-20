33eacc91085f6d781a70a3faf25939fec4b14e34
SamsterJam <samsterjam@gmail.com>
2024-05-20 14:42:18 -0400

    Added Create Product Screen

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -7,7 +7,7 @@ export default function MenuStack(){
     return <Stack screenOptions={{}}>
         <Stack.Screen name="index" options={{title: 'Menu'
           ,headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
