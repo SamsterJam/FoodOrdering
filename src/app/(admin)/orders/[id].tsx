d282d3a9587dc89a418fc4d7a48735eae9832f5f
SamsterJam <samsterjam@gmail.com>
2024-05-21 14:27:52 -0400

    Assignment #2, created orders screen

---
+++ src/app/(admin)/orders/[id].tsx
@@ -0,0 +1,26 @@
+import { View, Text, FlatList } from "react-native";
+import OrderListItem from "@/src/components/OrderListItem";
+import orders from "@/assets/data/orders";
+import { Stack, useLocalSearchParams } from "expo-router";
+import OrderItemListItem from "@/src/components/OrderItemListItem";
+
+export default function OrderDetailsScreen(){
+    const { id } = useLocalSearchParams();
+
+    const order = orders.find(order => order.id.toString() === id);
+    
+    if (!order) return <Text>Order not found!</Text>;
+
+    return(
+        <View>
+            <Stack.Screen options={{ title: `Order #${order.id}` }} />
+            <OrderListItem order={order}/>
+
+            <FlatList 
+                data={order.order_items}
+                renderItem={({item}) => <OrderItemListItem item={item} />}
+                contentContainerStyle={{padding: 5, gap: 5}}
+            />
+        </View>
+    )
+}
---
+++ src/components/OrderListItem.tsx
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Order } from "../types";
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { Link, useSegments } from "expo-router";
+
+dayjs.extend(relativeTime);
+
+
+
+type OrderListItemProps = {
+    order: Order;
+}
+
+export default function OrderListItem({order}: OrderListItemProps) {
+    const segments = useSegments();
+    return(
+        <Link href={`/${segments[0]}/orders/${order.id}`} asChild>
+            <Pressable style={styles.container}>
+                <View>
+                    <Text style={styles.orderText}>Order #{order.id}</Text>
+                    <Text style={styles.ageText}>{dayjs(order.created_at).fromNow()}</Text>
+                </View>
+                <Text style={[styles.statusText, {color: (order.status==="Delivered")? "gray" : "green"}]}>{order.status}</Text>
+            </Pressable>
+        </Link>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    orderText: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    ageText: {
+        color: 'gray',
+        fontSize: 16,
+    },
+    statusText: {
+        fontWeight: '500',
+        fontSize: 18,
+    },
+});
