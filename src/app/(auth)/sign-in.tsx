03f3cb396da05e85e58bd3247611f8e24f85f8b0
SamsterJam <samsterjam@gmail.com>
2024-05-23 13:58:54 -0400

    Integrated Supabase into login & sign up screens

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,16 +1,21 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native'
+import {View, Text, TextInput, StyleSheet, Alert} from 'react-native'
 import { useState } from 'react';
 import Button from '@/src/components/Button';
 import Colors from '@/src/constants/Colors';
 import { Link, Stack } from 'expo-router';
+import { supabase } from '@/src/lib/supabase';
 
 export default function SignInScreen(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState(''); 
+    const [loading, setLoading] = useState(false);
 
-    const onSignIn = () =>{
-
+    async function signInWithEmail(){
+        setLoading(true);
+        const {error} = await supabase.auth.signInWithPassword({email, password});
+        if (error) Alert.alert(error.message)
+        setLoading(false);
     }
 
     return(
@@ -23,7 +28,7 @@ export default function SignInScreen(){
             <TextInput autoComplete='password' secureTextEntry value={password} onChangeText={setPassword} style={styles.input}/>
 
             <Text style={{color: 'red'}}>{errors}</Text>
-            <Button onPress={onSignIn} text={"Sign in"}/>
+            <Button onPress={signInWithEmail} text={loading? "Signing in..." : "Sign in"}/>
             <Link href={"/sign-up"} asChild>
                 <Text style={styles.textButton}>Create an account</Text>
             </Link>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,16 +1,21 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native'
+import {View, Text, TextInput, StyleSheet, Alert} from 'react-native'
 import { useState } from 'react';
 import Button from '@/src/components/Button';
 import Colors from '@/src/constants/Colors';
 import { Link, Stack } from 'expo-router';
+import { supabase } from '@/src/lib/supabase';
 
 export default function SignInScreen(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState(''); 
+    const [loading, setLoading] = useState(false);
 
-    const onSignIn = () =>{
-
+    async function signUpWithEmail(){
+        setLoading(true);
+        const {error} = await supabase.auth.signUp({ email, password });
+        if (error) Alert.alert(error.message)
+        setLoading(false);
     }
 
     return(
@@ -23,7 +28,7 @@ export default function SignInScreen(){
             <TextInput autoComplete='password' secureTextEntry value={password} onChangeText={setPassword} style={styles.input}/>
 
             <Text style={{color: 'red'}}>{errors}</Text>
-            <Button onPress={onSignIn} text={"Create an account"}/>
+            <Button onPress={signUpWithEmail} disabled={loading} text={loading? "Creating account..." : "Create an account"}/>
             <Link href={"/sign-in"} asChild>
                 <Text style={styles.textButton}>Sign in</Text>
             </Link>
