dfd9a37e424c119ca355052adc650be517dca175
SamsterJam <samsterjam@gmail.com>
2024-05-24 15:11:11 -0400

    Switched to using ReactQuery

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,8 +1,22 @@
-import { View, FlatList} from 'react-native';
-import products from '@assets/data/products';
+import { View, FlatList, ActivityIndicator, Text} from 'react-native';
 import ProductListItem from '@components/ProductListItem';
+import { supabase } from '@/src/lib/supabase';
+import { useQuery } from '@tanstack/react-query';
+import { useProductList } from '@/src/api/products';
+
 
 export default function TabOneScreen() {
+
+  const {data: products, error, isLoading} = useProductList();
+
+  if(isLoading){
+    return <ActivityIndicator />;
+  }
+
+  if(error){
+    return <Text>Failed to fetch Products</Text>
+  }
+
   return (
       <FlatList 
         data={products}
